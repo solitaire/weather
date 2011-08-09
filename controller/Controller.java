8b51f3358a7a2a9e1c38f7617942291a2109543d
solitaire <solitaire@g.pl>
2011-08-09 17:42:07 +0200

    added blocking queue wrapper

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,6 +1,26 @@
 package controller;
 
+import queue.EventQueue;
+import view.View;
+import model.Model;
+
+/**
+ * Controller of the application
+ * 
+ * @author Anna Stępień
+ * @version 09-08-2011
+ */
 public class Controller 
 {
+	private final Model model;
+	private final View view;
+	private final EventQueue eventQueue;
+	
+	public Controller(Model model, View view, EventQueue eventQueue)
+	{
+		this.model = model;
+		this.view = view;
+		this.eventQueue = eventQueue;
+	}
 
 }
