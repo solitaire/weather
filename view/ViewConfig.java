131e83030fe16252c63fe3dd5ca26b922f985c4a
solitaire <solitaire@g.pl>
2011-08-10 12:02:55 +0200

    added basic data structures for weather information storage

---
+++ view/ViewConfig.java
@@ -0,0 +1,16 @@
+package view;
+
+/**
+ * View configuration parameters
+ * 
+ * @author Anna Stępień
+ * @version 09-08-2011
+ *
+ */
+class ViewConfig 
+{
+	
+	public final static int WIDTH = 400;
+	public final static int HEIGHT= 750;
+
+}
