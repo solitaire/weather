1f957d5769eaf4bbe9645fa4df418c584e51e68e
solitaire <solitaire@g.pl>
2011-08-10 12:02:55 +0200

    added basic data structures for weather information storage

--- view/View.java
+++ view/View.java
@@ -1,6 +1,41 @@
 package view;
 
+import javax.swing.SwingUtilities;
+
+/**
+ * View facade 
+ * 
+ * @author Anna Stępień
+ * @version 09-08-2011
+ *
+ */
 public class View 
 {
+	/** Main frame of the app */
+	private final MainFrame frame;
+	
+	/**
+	 * Default constructor
+	 */
+	public View()
+	{
+		frame = new MainFrame();
+	}
+
+	/**
+	 * Displays main window
+	 */
+	public void display() 
+	{
+		SwingUtilities.invokeLater(new Runnable() 
+		{
+			
+			@Override
+			public void run() 
+			{
+				frame.setVisible(true);
+			}
+		});
+	}
 
 }
