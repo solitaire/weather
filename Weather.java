696186a442b8949496b90e2bf8cf814141cb9976
solitaire <solitaire@g.pl>
2011-08-10 12:02:55 +0200

    added basic data structures for weather information storage

---
+++ Weather.java
@@ -0,0 +1,26 @@
+import controller.Controller;
+import queue.EventQueue;
+import view.View;
+import model.Model;
+
+
+/**
+ * 
+ * @author Anna Stępień
+ * @version 09-08-2011
+ *
+ */
+public class Weather 
+{
+	public static void main(String args[])
+	{
+		final Model model = new Model();
+		final View view = new View();
+		final EventQueue eventQueue = new EventQueue();
+		final Controller controller = new Controller(model, view, eventQueue);
+		
+		view.display();
+		controller.dispatch();
+	}
+
+}
--- model/Model.java
+++ model/Model.java
@@ -1,6 +1,21 @@
 package model;
 
+/**
+ * Model of the application
+ * 
+ * @author Anna Stępień
+ * @version 09-08-2011
+ *
+ */
 public class Model 
 {
+	
+	/**
+	 * Default constructor
+	 */
+	public Model()
+	{
+		
+	}
 
 }
